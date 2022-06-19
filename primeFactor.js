900fdbf29497a0585fb5369c0577ea905d58215e
ghector6 <ghectormus@gmail.com>
2022-06-19 18:02:34 -0500

    ending project

--- primeFactor.js
+++ primeFactor.js
@@ -1,15 +1,15 @@
-class primeFactor {
-    static getHighestPrime(range){
-        let primeNum = 2;
-        for (let i = 0; i <= range; i++){
-            if(range % primeNum === 0){
-                range /= primeNum;
+class primeFactor { //Creando clase con metodo static que va a generar la busqueda del factor primo ma grande
+    static getHighestPrime(range){  // declarando metodo estatico que toma un rango(numero) 
+        let primeNum = 2; //variable que empieza con el numero 2 como rango 
+        for (let i = 0; i <= range; i++){ // iterador que loopea mientras alcance el valor del rango
+            if(range % primeNum === 0){ // condicional que revisa si el numero es divisible entre el primeNum
+                range /= primeNum; //si es divisible se divide entre ese numero 
             }else{
-                primeNum++
+                primeNum++ //se suma si no el primeNum para seguir iterando 
             }        
         }
-        return primeNum; 
+        return primeNum; //regresa el numero primo mas alto que dividio la cifra
     }
 }
 
-module.exports = primeFactor
+module.exports = primeFactor // exportando usando Common JS  
--- primefactor.test.js
+++ primefactor.test.js
@@ -10,4 +10,8 @@ describe("test to se if i get highest prime factor", () => {
         const numTwNine = primeFactor.getHighestPrime(29);
         expect(numTwNine).toBe(29);
     })
+    test("to see if i get correct number for euler exercise", () => {
+        const bigNum = primeFactor.getHighestPrime(600851475143);
+        expect(bigNum).toBe(6857);
+    })
 })
