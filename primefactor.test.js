5419c7388bf8419f4f78dd9ae1aa154279defa83
ghector6 <ghectormus@gmail.com>
2022-06-19 16:56:14 -0500

    adding correct answers to tests to see if github gets jest with github actions

--- primefactor.test.js
+++ primefactor.test.js
@@ -2,12 +2,12 @@ const primeFactor = require("./primeFactor");
 
 describe("test to se if i get highest prime factor", () => {
     test("to get the 7 as prime factor of 7", () => {
-        const numSeven = primeFactor.getHighestPrime(8);
+        const numSeven = primeFactor.getHighestPrime(7);
         expect(numSeven).toBe(7);
 
     })
     test("to se if i get 2 length prime number", () => {
-        const numTwNine = primeFactor.getHighestPrime(27);
+        const numTwNine = primeFactor.getHighestPrime(29);
         expect(numTwNine).toBe(29);
     })
 })
