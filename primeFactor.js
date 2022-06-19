2afb5f4741fc734bb73e3ef6de375196c9382361
ghector6 <ghectormus@gmail.com>
2022-06-19 16:52:00 -0500

    adding method to get highest prime factor

---
+++ primeFactor.js
@@ -0,0 +1,15 @@
+class primeFactor {
+    static getHighestPrime = (range) => {
+        let primeNum = 2;
+        for (let i = 0; i <= range; i++){
+            if(range % primeNum === 0){
+                range /= primeNum;
+            }else{
+                primeNum++
+            }        
+        }
+        return primeNum; 
+    }
+}
+
+module.exports = primeFactor
