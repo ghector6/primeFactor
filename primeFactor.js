aa45478158f93d09019cb1a749e128cc4bfcd6a3
ghector6 <ghectormus@gmail.com>
2022-06-26 10:39:23 -0500

    Adding while loop to solve the problem, THANKS ERICK!!!

--- primeFactor.js
+++ primeFactor.js
@@ -1,7 +1,7 @@
 class primeFactor { //Creando clase con metodo static que va a generar la busqueda del factor primo ma grande
     static getHighestPrime(range){  // declarando metodo estatico que toma un rango(numero) 
         let primeNum = 2; //variable que empieza con el numero 2 como rango 
-        for (let i = 0; i <= range; i++){ // iterador que loopea mientras alcance el valor del rango
+        while (range !=1){ // iterador que loopea mientras alcance el valor del rango
             if(range % primeNum === 0){ // condicional que revisa si el numero es divisible entre el primeNum
                 range /= primeNum; //si es divisible se divide entre ese numero 
             }else{
